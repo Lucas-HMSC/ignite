e3234d14b2b703c51ce6f8753099e5f713e79a20
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-12-01 21:28:20 -0300

    Conceitos importantes
    Aulas assistidas: Primeiro componente React, Propriedades no React, Estado do componente, A imutabilidade no React, Fast Refresh no Webpack.

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,7 @@
+import { RepositoryFunction } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App() {
-  return <h1>Hello World!</h1>
+  return <RepositoryFunction />
 }
