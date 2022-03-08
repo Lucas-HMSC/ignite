c9b9d097d75400bfd9b656a03e050e1156fb4530
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-03-08 19:36:47 -0300

    Páginas estáticas
    Aulas assistidas: Gerando previews estáticos, finalização do módulo.

--- reactjs/03-ignews/src/pages/posts/preview/[slug].tsx
+++ reactjs/03-ignews/src/pages/posts/preview/[slug].tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async  () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -84,6 +84,7 @@ export const getStaticProps: GetStaticProps = async({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    revalidate: 60 * 30, // 30 minutes
   }
 }
