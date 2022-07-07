982f3e2b231b3e8fda9abb557b9dafaa5d44f01b
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-07-06 21:27:28 -0300

    Estrutura da aplicação
    Aulas assistidas: Componente: Sidebar, Finalizando Sidebar, Componente: Post, Estilizações do Post.

--- reactjs/01-ignite-feed/src/App.jsx
+++ reactjs/01-ignite-feed/src/App.jsx
@@ -1,4 +1,8 @@
+import { Sidebar } from './components/Sidebar';
 import { Header } from './components/Header';
+import { Post } from './components/Post';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -6,6 +10,14 @@ export function App() {
   return (
     <div>
       <Header />
+
+      <div className={ styles.wrapper }>
+        <Sidebar />
+        
+        <main>
+           <Post />
+        </main>
+      </div>
     </div>
   )
 }
